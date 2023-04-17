2a09ef68549420b16de3b046e2801ce5bd05a298
m1n9o <gxmk9999@gmail.com>
2023-04-17 10:14:59 +0800

    correct func to get the stack

--- s3-bucket-operator/sync.py
+++ s3-bucket-operator/sync.py
@@ -14,15 +14,12 @@ class Controller(BaseHTTPRequestHandler):
         app = App()
         stack = Stack(app, s3_bucket.name)
 
-        # Create an S3 bucket based on the parent's parameters
         Bucket(
             stack,
             f"Bucket{s3_bucket.account_id}{s3_bucket.name}",
             bucket_name=s3_bucket.name
         )
 
-        # Generate the desired CloudFormation template
-        cloudformation_template = app.synth().get_stack(stack.stack_name).template
+        cloudformation_template = app.synth().get_stack_by_name(stack.stack_name).template
 
-        # Return the CloudFormation template as the desired state
         return {"template": cloudformation_template.to_json()}
